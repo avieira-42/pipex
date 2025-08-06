3f9e90d6bcae53e0de227069612b687a48956d08
avieira-42 <vieiramendesalex@gmail.com>
2025-08-06 01:35:18 +0100

    matrix realloc

--- practice.c
+++ practice.c
@@ -6,7 +6,7 @@
 /*   By: a-soeiro <avieira-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 02:44:21 by a-soeiro          #+#    #+#             */
-/*   Updated: 2025/08/04 04:17:45 by a-soeiro         ###   ########.fr       */
+/*   Updated: 2025/08/05 16:08:55 by avieira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ typedef struct s_cmd
 	int		j;
 	char	**env;
 	char	**dirs;
-	char	*args[2];
+	char	**args;
 	char	*path;
 	pid_t	pid;
 }	t_cmd;
@@ -74,18 +74,48 @@ char *get_path(char **dirs, char *cmd)
 	return (NULL);
 }
 
+int	ft_matrix_size(char **split)
+{
+	int	i;
+
+	i = 0;
+	while (split[i])
+		i++;
+	return (i);
+}
+
+void	ft_realloc_matrix(char ***split, int size)
+{
+	ft_free_matrix
+	*split = malloc((sizeof(char *) * (size + 1)));
+}
+
+void	join_argv(char ***split)
+{
+	int		i;
+	int		size;
+	char	**split_ptr;
+
+	i = 0;
+	size = ft_matrix_size(*split);
+	split_ptr = ft_matrixdup(*split);
+	ft_realloc_matrix(split);
+	while (i < size)
+	{
+	}
+	ft_free_matrix(split_ptr);
+}
+
 int	pipex_init(t_cmd *cmd1, t_cmd *cmd2, char **argv, char **env)
 {
 	cmd1->i = 0;
 	cmd1->j = 1;
 	cmd2->i = 1;
 	cmd2->j = 0;
-	cmd1->args[0] = (ft_strdup(argv[1]));
-	cmd2->args[0] = (ft_strdup(argv[2]));
+	cmd1->args = ft_split(argv[1], ' ');
+	cmd2->args = ft_split(argv[4], ' ');
 	if (!cmd1->args || !cmd2->args)
 		return (0);
-	cmd1->args[1] = NULL;
-	cmd2->args[1] = NULL;
 	cmd1->env = env;
 	cmd2->env = env;
 	cmd1->path = get_path_env(env);
@@ -116,28 +146,36 @@ int	get_pid(int *fd, t_cmd *cmd)
 		else if (cmd->j == 0)
 			dup2(fd[cmd->j], STDIN_FILENO);
 		close(fd[cmd->j]);
-		execve(cmd->path, cmd->args, cmd->env);
-
+		execve(cmd->args, cmd->path, cmd->env);
 		perror("execve failed");
 		exit(1);
 	}
 	return (1);
 }
 
+void	free_cmd_structs(t_cmd cmd1, t_cmd cmd2)
+{
+	ft_free_matrix(cmd1.dirs);
+	free(cmd1.path);
+	free(cmd2.path);
+	ft_free_matrix(cmd1.args);
+	ft_free_matrix(cmd2.args);
+}
+
 int main(int argc, char **argv, char **env)
-{	
-	int		err_ctrl;
+{
+	int		error_ctrl;
 	int		fd[2];
 	t_cmd	cmd1;
 	t_cmd	cmd2;
 
-	if (argc != 3)
+	if (argc != 5)
 		return (1);
-	err_ctrl = pipex_init(&cmd1, &cmd2, argv, env);
-	if (!err_ctrl)
+	error_ctrl = pipex_init(&cmd1, &cmd2, argv, env);
+	if (!error_ctrl)
 		return (2);
-	err_ctrl = pipe(fd);
-	if (err_ctrl == -1)
+	error_ctrl = pipe(fd);
+	if (error_ctrl == -1)
 		return (3);
 	if (!get_pid(fd, &cmd1) || !get_pid(fd, &cmd2))
 		return (4);
@@ -145,7 +183,5 @@ int main(int argc, char **argv, char **env)
 	close(fd[1]);
 	waitpid(cmd1.pid, NULL, 0);
 	waitpid(cmd2.pid, NULL, 0);
-	
-	ft_free_matrix(cmd1.dirs);
-	free(cmd1.path);
+	free_cmd_structs(cmd1, cmd2);
 }
